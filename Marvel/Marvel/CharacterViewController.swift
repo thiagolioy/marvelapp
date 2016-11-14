249d3e9a8642d9a14509b33266d517e8f64f8f93
Thiago Lioy <Thiago.Lioy@CSSP-580.local>
2016-11-14 15:04:07 -0200

    Creating initial view controllers

---
+++ Marvel/Marvel/CharacterViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CharacterViewController.swift
+//  Marvel
+//
+//  Created by Thiago Lioy on 14/11/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+import UIKit
+
+class CharacterViewController: UIViewController {
+    
+}
+
---
+++ Marvel/Marvel/CharactersViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CharactersViewController.swift
+//  Marvel
+//
+//  Created by Thiago Lioy on 14/11/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+
+}
