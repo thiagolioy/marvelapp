8910742023161e449ba160ab63ddfb997e002af8
Thiago Lioy <Thiago.Lioy@CSSP-580.local>
2016-11-14 14:37:10 -0200

    Creating initial project structure

---
+++ Marvel/Marvel/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Marvel
+//
+//  Created by Thiago Lioy on 14/11/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
