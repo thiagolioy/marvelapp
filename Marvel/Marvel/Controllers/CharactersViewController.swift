6fb13db4722b81527e5b32919a79724bf5dc4a2d
Thiago Lioy <Thiago.Lioy@CSSP-580.local>
2016-11-15 10:40:24 -0200

    project changes

--- Marvel/Marvel/Controllers/CharactersViewController.swift
+++ Marvel/Marvel/Controllers/CharactersViewController.swift
@@ -9,5 +9,11 @@
 import UIKit
 
 class CharactersViewController: UIViewController {
+    let apiManager = MarvelAPIManager()
+}
 
+extension CharactersViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
 }
