5edcb018f90cc94e3c83cd814a0573296f1a2d52
Thiago Lioy <Thiago.Lioy@CSSP-580.local>
2016-11-26 23:34:44 -0200

    Tested model's structs Added new test pods to Podfile Added Helper class to load fixtures on test target Added json mocks

---
+++ Marvel/MarvelTests/MockLoader.swift
@@ -0,0 +1,40 @@
+//
+//  MockLoader.swift
+//  Marvel
+//
+//  Created by Thiago Lioy on 26/11/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+@testable import Marvel
+
+struct MockLoader {
+    
+    private let data: Data
+    private let json: String
+    
+    init?(file: String, withExtension fileExt: String = "json", in bundle:Bundle = Bundle.main) {
+        guard let path = bundle.path(forResource: file, ofType: fileExt) else {
+            return nil
+        }
+        let pathURL = URL(fileURLWithPath: path)
+        do {
+            data = try Data(contentsOf: pathURL, options: .dataReadingMapped)
+            if let decoded = NSString(data: data, encoding: 0) as? String {
+                json = decoded
+            } else {
+                return nil
+            }
+        } catch{
+            return nil
+        }
+    }
+    
+    
+    func map<T: Mappable>(to type: T.Type) -> T? {
+        return Mapper<T>().map(JSONString: json)
+    }
+    
+}
