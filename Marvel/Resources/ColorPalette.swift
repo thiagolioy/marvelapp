3cbc8e27e93711d2b5a6963be4b96f27b5ebba2a
Thiago Lioy <Thiago.Lioy@cssp-580.localdomain>
2016-12-11 14:47:17 -0200

    Begin to implement new layout

---
+++ Marvel/Resources/ColorPalette.swift
@@ -0,0 +1,18 @@
+//
+//  ColorPalette.swift
+//  Marvel
+//
+//  Created by Thiago Lioy on 11/12/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+
+import UIKit
+enum ColorPalette {
+    
+    static let red =  UIColor(red:0.910, green: 0.282, blue: 0.333, alpha: 1.000)
+    
+    static let white =  UIColor(red:1.000, green: 0.988, blue: 0.976, alpha: 1.000)
+    
+    static let black =  UIColor(red:0.251, green: 0.247, blue: 0.298, alpha: 1.000)
+}
