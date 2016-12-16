82e7d1a407eda4663b3975d5335e17eebc0bebf0
Rodrigo Cavalcante <Rodrigo.Cavalcante@CSSP-567.local>
2016-12-16 14:21:57 -0200

    add SearchCollectionCell and SearchCollectionController

---
+++ Marvel/Cells/SearchCollectionCell.swift
@@ -0,0 +1,21 @@
+//
+//  SearchCollectionCell.swift
+//  Marvel
+//
+//  Created by Rodrigo Cavalcante on 15/12/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+import UIKit
+import Reusable
+
+class SearchCollectionCell: UICollectionViewCell, NibReusable {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
