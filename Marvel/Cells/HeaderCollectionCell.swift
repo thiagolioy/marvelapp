a38adbcde7b7950ed8e707b7d8b9938737e68ca8
Rodrigo Cavalcante <Rodrigo.Cavalcante@CSSP-567.local>
2016-12-16 14:17:16 -0200

    add HeaderCollectionCell

---
+++ Marvel/Cells/HeaderCollectionCell.swift
@@ -0,0 +1,29 @@
+//
+//  HeaderCollectionCell.swift
+//  Marvel
+//
+//  Created by Rodrigo Cavalcante on 15/12/16.
+//  Copyright © 2016 Thiago Lioy. All rights reserved.
+//
+
+import UIKit
+import Reusable
+
+class HeaderCollectionCell: UICollectionViewCell, NibReusable, Charactable {
+
+    @IBOutlet weak var picture: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+    func setup(character: Character) {
+        self.picture.image = nil
+        
+        if let thumbImage = character.thumImage {
+            self.picture.download(image: thumbImage.fullPath())
+            self.name.text = character.name
+        }
+    }
+    
+    override func prepareForReuse() {
+        self.picture.cancelDownload()
+    }
+}
